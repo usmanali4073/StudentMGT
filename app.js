2bf884eee1a257c0d56f1ca426edf091d1658e83
usman ali <usmanali4073@gmail.com>
2019-12-05 21:59:55 -0500

    Added Post

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const port = 3000
 const bookRouter = express.Router();
 const mongoose = require('mongoose');
 const book = require("./Models/bookModel")
+const swaggerUi = require('swagger-ui-express');
+const bodyParser = require('body-parser')
+    // const swaggerDocument = require('./swagger.json');
 
 //Middleware
 app.use(morgan('dev'))
-
+    // parse application/json
+app.use(bodyParser.urlencoded())
+app.use(bodyParser.json())
+    // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongoose.Promise = global.Promise;
 
@@ -32,6 +38,7 @@ bookRouter.route("/book")
         book.find(query, (err, books) => {
             if (err) {
                 console.log(`Error: ` + err)
+                return res.send(err)
             } else {
                 if (books.length === 0) {
                     console.log("message")
@@ -61,6 +68,14 @@ bookRouter.route("/book/:bookId")
                 }
             }
         });
+    })
+
+
+bookRouter.route("/book")
+    .post((req, res) => {
+        let bk = new book(req.body);
+        bk.save();
+        return res.status(201).json(bk)
     })
 
 app.use("/api", bookRouter)
